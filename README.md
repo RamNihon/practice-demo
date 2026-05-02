3882106c372bb2c1babc52af11ca45c89562006f
Ram Kumar <ramkrkumar2000@gmail.com>
2026-05-02 08:25:22 +0530

    Adding New Files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # practice-demo
 This is a demo repo for Git.
 
-#Student
+# Student
 Ram kumar.
---
+++ app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",()=>{
+let p = document.createElement("h2");
+p.innerText="Welcome to my Github account. Have a great day ahed !";
+document.body.append(p);
+})
+
